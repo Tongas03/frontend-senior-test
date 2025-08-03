4dc545672bb70f91fcdea00a0c05297c2a474c1d
Gastón <gaston.rai28@gmail.com>
2025-08-03 20:44:53 -0300

    add first test

--- app/__tests__/home.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-
-describe('Pantalla Home', () => {
-  it('muestra el texto esperado', () => {
-    render(<div>Wallet</div>)
-    expect(screen.getByText('Wallet')).toBeInTheDocument()
-  })
-})
